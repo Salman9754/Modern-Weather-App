f46d478acecc75683fd694f58c9e6e23713f2f5d
Muhammad Salman Ansar <salmanwattoo79@gmail.com>
2025-01-07 23:01:08 +0500

    Data fetched
    Data fetched from weather API

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 $(document).ready(function () {
     $(".owl-carousel").owlCarousel({
-      items: 1, // Default number of items
+      items: 1, 
       margin: 10,
-      loop: true, // Enable infinite looping
-      nav: true, // Show navigation arrows
-      dots: true, // Show navigation dots
+      loop: true, 
+      nav: true, 
+      dots: true, 
       responsive: {
-        0: { items: 3 }, // 1 item on small screens
-        600: { items: 4 }, // 2 items on medium screens
-        1000: { items: 8 } // 3 items on larger screens
+        0: { items: 3 }, 
+        600: { items: 4 }, 
+        1000: { items: 8 } 
       }
     });
   });
+
+  async function weather() {
+    const url = 'http://api.weatherapi.com/v1/current.json?key=5508d1849c974c9a929165810250701&q=Karachi&aqi=yes';
+    try {
+      let data = await fetch(url)
+      let dataJson = await data.json()
+      console.log(dataJson);
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
+  weather()
