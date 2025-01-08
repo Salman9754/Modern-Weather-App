b784f3ebc1dd01ceab2723874fe2ce5f414d2fb1
Muhammad Salman Ansar <salmanwattoo79@gmail.com>
2025-01-08 22:07:40 +0500

    update app.js
    Manipulated DOM with API response

--- app.js
+++ app.js
@@ -23,14 +23,16 @@ function myAlert(icon, title, text) {
   });
 }
 
-window.onload = function currentLocation() {
-  if (navigator) {
+function currentLocation() {
+  if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(getPosition, showError);
   }
-};
-document.getElementById('currentLocation').addEventListener('click',currentLocation())
+}
+
+window.onload = currentLocation;
+document.getElementById("current").addEventListener("click", currentLocation);
+
 async function getPosition(data) {
-  alert('ok')
   let lat = await data.coords.latitude;
   let lon = await data.coords.longitude;
   weatherByLocation(lat, lon);
@@ -38,25 +40,29 @@ async function getPosition(data) {
 
 function showError(error) {
   console.log(error);
+  myAlert("error", "Oops...", "Location not found enter manually");
 }
 
 async function weatherByLocation(lat, lon) {
-  const url = `https://api.weatherapi.com/v1/forecast.json?key=5508d1849c974c9a929165810250701&q=${lat},${lon}&aqi=yes&days=6&hour=1`;
+  const url = `https://api.weatherapi.com/v1/forecast.json?key=5508d1849c974c9a929165810250701&q=${lat},${lon}&aqi=yes&days=6`;
   try {
     Swal.showLoading();
     const response = await fetch(url);
     const responseJson = await response.json();
-    console.log(responseJson);
+    showWeather(responseJson);
+    if(response.ok){
+      swal.close()
+    }
   } catch (error) {
     console.log(error);
-  } finally {
-    swal.close();
+    myAlert("error", "Oops...", "Location not found enter manually");
   }
 }
 
 function search() {
   const city = document.getElementById("searchInput").value;
   if (city) {
+    document.getElementById("searchInput").blur();
     weatherByCity(city);
   }
 }
@@ -69,15 +75,82 @@ document
   });
 
 async function weatherByCity(city) {
-  const url = `http://api.weatherapi.com/v1/current.json?key=5508d1849c974c9a929165810250701&q=${city}&aqi=yes&days=6&hour=1`;
+  const url = `https://api.weatherapi.com/v1/forecast.json?key=5508d1849c974c9a929165810250701&q=${city}&aqi=yes&days=6`;
   try {
     Swal.showLoading();
     const response = await fetch(url);
     const responseJson = await response.json();
-    console.log(responseJson);
+    showWeather(responseJson);
+    if(response.ok){
+      swal.close()
+    }
   } catch (error) {
     console.log(error);
-  } finally {
-    swal.close();
+    myAlert("error", "Oops...", "Location not found");
+  } 
+}
+function showWeather(data) {
+  console.log(data);
+  let temperature = document.getElementById("temp");
+  temperature.innerHTML = `${Math.round(
+    data.current.temp_c
+  )} <sup style="font-size: 18px;">&#176C</sup>`;
+  let condition = document.getElementById("condition");
+  condition.innerHTML = data.current.condition.text;
+  let conditionImg = document.getElementById("conditionImg");
+  if (data.current.condition.text === "Clear") {
+    conditionImg.src = "Assets/moon-svgrepo-com.svg";
+  } else {
+    conditionImg.src = data.current.condition.icon;
+  }
+  let date = document.getElementById("date");
+  date.innerHTML = data.forecast.forecastday[0].date;
+
+  let city = document.getElementById("city");
+  city.innerHTML = data.location.name;
+  let day1 = document.getElementById("day1");
+  let date1 = document.getElementById("date1");
+  let apiDate1 = data.forecast.forecastday[4].date;
+  let dateObj = new Date(apiDate1);
+  day1.innerHTML = dateObj.toLocaleDateString("en-US", { weekday: "short" });
+  date1.innerHTML = dateObj.toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "short",
+  });
+  let day1Icon = document.getElementById("day1Icon");
+  if (data.forecast.forecastday[1].day.condition.text === "Clear") {
+    day1Icon.src = "Assets/moon-svgrepo-com.svg";
+  } else {
+    day1Icon.src = data.forecast.forecastday[1].day.condition.icon;
+  }
+  let day2 = document.getElementById("day2");
+  let date2 = document.getElementById("date2");
+  let apiDate2 = data.forecast.forecastday[2].date;
+  dateObj = new Date(apiDate2);
+  day2.innerHTML = dateObj.toLocaleDateString("en-US", { weekday: "short" });
+  date2.innerHTML = dateObj.toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "short",
+  });
+  let day2Icon = document.getElementById("day2Icon");
+  if (data.forecast.forecastday[2].day.condition.text === "Clear") {
+    day2Icon.src = "Assets/moon-svgrepo-com.svg";
+  } else {
+    day2Icon.src = data.forecast.forecastday[2].day.condition.icon;
+  }
+  let day3 = document.getElementById("day3");
+  let date3 = document.getElementById("date3");
+  let apiDate3 = data.forecast.forecastday[3].date;
+  dateObj = new Date(apiDate3);
+  day3.innerHTML = dateObj.toLocaleDateString("en-US", { weekday: "short" });
+  date3.innerHTML = dateObj.toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "short",
+  });
+  let day3Icon = document.getElementById("day3Icon");
+  if (data.forecast.forecastday[3].day.condition.text === "Clear") {
+    day3Icon.src = "Assets/moon-svgrepo-com.svg";
+  } else {
+    day3Icon.src = data.forecast.forecastday[3].day.condition.icon;
   }
 }
